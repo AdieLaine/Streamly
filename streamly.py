e7f0358ff97e6f902549d3e34a74232e5abc03a9
Madie Laine <106930581+AdieLaine@users.noreply.github.com>
2024-07-24 16:31:55 -0400

    Update to GPT-4o-mini
    GPT Model Update

--- streamly.py
+++ streamly.py
@@ -138,7 +138,7 @@ def on_chat_submit(chat_input, api_key, latest_updates, use_langchain=False):
     user_input = chat_input.strip().lower()
 
     # Initialize the OpenAI API
-    model_engine = "gpt-3.5-turbo-0125"
+    model_engine = "gpt-4o-mini-2024-07-18"
 
     # Initialize the conversation history with system and assistant messages
     if 'conversation_history' not in st.session_state:
